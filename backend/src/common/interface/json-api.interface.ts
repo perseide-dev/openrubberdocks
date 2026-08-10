6addff09eb66bd6870db48ba1ca5149f5a3f5e06
perseide-dev <miguelvg097@gmail.com>
2026-08-10 16:30:59 -0500

    feat: define JsonApiResource and JsonApiResponse interfaces for standardized API responses

---
+++ backend/src/common/interface/json-api.interface.ts
@@ -0,0 +1,15 @@
+export interface JsonApiResource<T> {
+    type: string;
+    id: string;
+    attributes: Omit<T, 'id'>;
+    relationships?: Record<string, any>
+    links?: Record<string, string>
+}
+
+export interface JsonApiResponse<T> {
+    data: JsonApiResource<T> | JsonApiResource<T>[]
+    included?: JsonApiResource<any>[]
+    meta?: Record<string, any>
+    links?: Record<string, string>
+    jsonapi?: { version: string }
+}
